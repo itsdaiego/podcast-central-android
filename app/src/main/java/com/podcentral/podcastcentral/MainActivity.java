6132030144c3670548d21f375df0ba303de9a5ac
Dyego Rodrigo <dyego9444@hotmail.com>
2015-07-19 19:59:13 -0300

    setting up a simple toolbar

--- app/src/main/java/com/podcentral/podcastcentral/MainActivity.java
+++ app/src/main/java/com/podcentral/podcastcentral/MainActivity.java
@@ -2,15 +2,19 @@ package com.podcentral.podcastcentral;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
